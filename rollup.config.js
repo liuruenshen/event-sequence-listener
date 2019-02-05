399ef95f7111fe0217b0f27a371423c9172408aa
dustin71728 <kiwi71728@icloud.com>
2019-02-05 20:42:28 +0800

    chore: clean up the dist folder

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,11 @@ import * as path from 'path'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+import cleaner from 'rollup-plugin-cleaner'
 
 import pkg from './package.json'
+
+const dist = 'dist'
 
 export default commandLineArgs => {
   const plugins = [
@@ -19,7 +22,6 @@ export default commandLineArgs => {
   ]
 
   const input = 'src/index.ts'
-  const dist = 'dist'
 
   return [
     {
@@ -28,7 +30,9 @@ export default commandLineArgs => {
         file: path.resolve(dist, pkg.module),
         format: 'esm'
       },
-      plugins
+      plugins: plugins.concat([cleaner({
+        targets: [dist]
+      })])
     },
     {
       input,
