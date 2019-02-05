8f5e07fc574a66edcb79b78fa64cc70e52550fc1
dustin71728 <kiwi71728@icloud.com>
2019-02-05 19:43:30 +0800

    chore: build the bundle successfully

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,51 @@
+import * as path from 'path'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 
+import pkg from './package.json'
+
 export default commandLineArgs => {
+  const plugins = [
+    babel({
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.ts']
+    }),
+    resolve({
+      module: true,
+      extensions: ['.mjs', '.js', '.ts', 'json']
+    }),
+    commonjs()
+  ]
+
+  const input = 'src/index.ts'
+  const dist = 'dist'
+
   return [
     {
-      input: 'src/index.js',
+      input,
       output: {
-        name: 'event-order-es.js',
+        file: path.resolve(dist, pkg.module),
         format: 'esm'
       },
-      plugins: [
-        resolve(),
-        commonjs(),
-        babel()
-      ]
+      plugins
     },
+    {
+      input,
+      output: {
+        file: path.resolve(dist, pkg.main),
+        format: 'cjs'
+      },
+      plugins
+    },
+    {
+      input,
+      output: {
+        file: path.resolve(dist, pkg.browser),
+        name: 'EventSequenceListener',
+        format: 'iife'
+      },
+      plugins
+    }
   ]
 }
