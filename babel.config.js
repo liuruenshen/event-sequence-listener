ef799f519c20ce76d729014d61e07b7fe923429d
dustin71728 <kiwi71728@icloud.com>
2019-02-05 19:54:01 +0800

    chore: attach babel-plugin-lodash

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,7 @@ module.exports = function (api) {
       "@babel/preset-typescript",
     ],
     plugins: [
+      'lodash',
       "@babel/proposal-class-properties",
       "@babel/plugin-proposal-object-rest-spread",
     ]
