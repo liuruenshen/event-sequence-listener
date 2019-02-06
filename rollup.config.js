4de791365177673c0892f06ad824742834ff160c
dustin71728 <kiwi71728@icloud.com>
2019-02-06 11:09:29 +0800

    test: configure rollup to create the bundle for testing

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,15 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import cleaner from 'rollup-plugin-cleaner'
+import multiEntry from "rollup-plugin-multi-entry"
 
 import pkg from './package.json'
 
 const dist = 'dist'
+const test = path.dirname(pkg.testBundle)
 
 export default commandLineArgs => {
+
   const plugins = [
     babel({
       exclude: 'node_modules/**',
@@ -16,40 +19,70 @@ export default commandLineArgs => {
     }),
     resolve({
       module: true,
-      extensions: ['.mjs', '.js', '.ts', 'json']
-    }),
-    commonjs()
+      extensions: ['.mjs', '.js', '.ts', 'json'],
+      preferBuiltins: true
+    })
   ]
 
   const input = 'src/index.ts'
 
-  return [
-    {
-      input,
-      output: {
-        file: path.resolve(dist, pkg.module),
-        format: 'esm'
-      },
-      plugins: plugins.concat([cleaner({
+  if (commandLineArgs.configPurpose === 'test') {
+    const pluginsForTest = plugins.concat([
+      commonjs({
+        namedExports: {
+          'should-util': ['hasOwnProperty']
+        }
+      }),
+      cleaner({ targets: [test] }),
+      multiEntry()
+    ])
+
+    return [
+      {
+        input: 'src/**/*.spec.ts',
+        output: {
+          file: pkg.testBundle,
+          format: 'cjs',
+        },
+        external: ['events'],
+        plugins: pluginsForTest
+      }
+    ]
+  }
+  else {
+    const pluginsForDist = plugins.concat([commonjs()])
+    const pluginsWitchCleanUp = pluginsForDist.concat([
+      cleaner({
         targets: [dist]
-      })])
-    },
-    {
-      input,
-      output: {
-        file: path.resolve(dist, pkg.main),
-        format: 'cjs'
+      })
+    ])
+
+    return [
+      {
+        input,
+        output: {
+          file: path.resolve(dist, pkg.module),
+          format: 'esm'
+        },
+        plugins: pluginsWitchCleanUp
       },
-      plugins
-    },
-    {
-      input,
-      output: {
-        file: path.resolve(dist, pkg.browser),
-        name: 'EventSequenceListener',
-        format: 'iife'
+      {
+        input,
+        output: {
+          file: path.resolve(dist, pkg.main),
+          format: 'cjs'
+        },
+        plugins: pluginsForDist
       },
-      plugins
-    }
-  ]
+      {
+        input,
+        output: {
+          file: path.resolve(dist, pkg.browser),
+          name: 'EventSequenceListener',
+          format: 'iife'
+        },
+        plugins: pluginsForDist
+      }
+    ]
+  }
 }
