b31eeeed5b344421c13d2dfef73cf807ac22215c
Liu Ruen-shen <kiwi71728@icloud.com>
2019-02-08 16:32:05 +0800

    chore: use the specific branch to update version in travis

--- deploy/update_version.sh
+++ deploy/update_version.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-git checkout -f "${TRAVIS_BRANCH}"
+if [[ -z $TRAVIS_PULL_REQUEST_BRANCH ]]; then
+	exit 0;
+fi
+
+git checkout -f "${TRAVIS_PULL_REQUEST_BRANCH}"
 
 npm run release
 
-git push "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}" --follow-tags
+git push "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "${TRAVIS_PULL_REQUEST_BRANCH}" --follow-tags
