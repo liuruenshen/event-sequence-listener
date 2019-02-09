cac1214b3c99169f3b443cd5411bdfa5f422b022
Liu Ruen-shen <kiwi71728@icloud.com>
2019-02-09 23:40:45 +0800

    chore(UPDATE VERSION): fecth the branch before checking out the branch

--- deploy/update_version.sh
+++ deploy/update_version.sh
@@ -6,6 +6,8 @@ if [[ -z $TRAVIS_PULL_REQUEST_BRANCH ]]; then
 	exit 0;
 fi
 
+git fetch origin "${TRAVIS_PULL_REQUEST_BRANCH}:${TRAVIS_PULL_REQUEST_BRANCH}"
+
 git checkout -f "${TRAVIS_PULL_REQUEST_BRANCH}"
 
 npm run release
