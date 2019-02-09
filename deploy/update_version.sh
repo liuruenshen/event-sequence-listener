bd53f1e876c7bc016c327e1f13debe4ad051f344
Liu Ruen-shen <kiwi71728@icloud.com>
2019-02-09 23:59:45 +0800

    chore: fix updating version process

--- deploy/update_version.sh
+++ deploy/update_version.sh
@@ -2,14 +2,12 @@
 
 set -e
 
-if [[ -z $TRAVIS_PULL_REQUEST_BRANCH ]]; then
+if [[ -z $TRAVIS_BRANCH ]]; then
 	exit 0;
 fi
 
-git fetch origin "${TRAVIS_PULL_REQUEST_BRANCH}:${TRAVIS_PULL_REQUEST_BRANCH}"
-
-git checkout -f "${TRAVIS_PULL_REQUEST_BRANCH}"
+git checkout -f "${TRAVIS_BRANCH}"
 
 npm run release
 
-git push "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "${TRAVIS_PULL_REQUEST_BRANCH}" --follow-tags
+git push "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}" --follow-tags
